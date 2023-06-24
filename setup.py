14fd4d0c57e48bb062b372e2135fba54ae75948e
knaiara <naiara.korta@posteo.net>
2023-06-24 10:55:40 +0200

    minor changes notebooks

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="attomo",
-    version="0.0.1",
+    version="0.1.0",
     description="A tool for pulse-echo ultrasound attenuation tomography",
     author="Naiara Korta Martiartu",
     author_email="naiara.korta@unibe.ch",
