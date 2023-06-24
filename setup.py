2fb46c450e61b2d53e70a5da74f5e99e3b4a8fe6
knaiara <naiara.korta@posteo.net>
2023-06-24 10:39:48 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="attomo",
+    version="0.0.1",
+    description="A tool for pulse-echo ultrasound attenuation tomography",
+    author="Naiara Korta Martiartu",
+    author_email="naiara.korta@unibe.ch",
+    url="https://github.com/naiarako/attomo",
+    license='GPL-3.0',
+    keywords=["ultrasound attenuation", "pulse-echo ultrasound", "tomography"],
+    packages=find_packages(),
+)
+
